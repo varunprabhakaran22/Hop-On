d3bc59eaad1506302d70d1efad004e0923b69774
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-25 18:27:46 +0530

    new component

--- public/src/components/TakeARide.js
+++ public/src/components/TakeARide.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
-import "../styles/takeARide"
+import "../styles/takeARide.css"
+
 
 class TakeARide extends Component{
     constructor(){
@@ -8,7 +9,18 @@ class TakeARide extends Component{
 
     render(){
         return(
-            
+            <div className = "login-page">
+                <div className = "login" > 
+                    <h1 className = "app-name"> Hop On </h1>
+                    <input type="text" className = "pick-up" placeholder = "Pick up Location" />
+                    <input type="text" className = "drop" placeholder = "Drop Location" />
+                </div>
+                <div>
+                    <input type = "button" className = " search-for-partner" value = "Search for Ride"/>
+                </div>
+            </div>
         )
     }
 }
+
+export default TakeARide;
