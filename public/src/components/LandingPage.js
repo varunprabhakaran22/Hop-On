699bdc153e4e3f5394fcf0fc79bbed526e6dd01e
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-25 18:27:23 +0530

    redirecting to other component

---
+++ public/src/components/LandingPage.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+import "../styles/landingPage.css"
+import { withRouter } from 'react-router-dom'
+class LandingPage extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+            <div className = "landing-page">
+                <div className = "landing" > 
+                    <h1 className = "app-name"> Hop On </h1>
+                    <button type="button" className = "take-ride" onClick={()=>this.props.history.push("/takearide")}>Take a <br />Ride</button>
+                    <button type="button" className = "share-ride" >Share a <br />Ride</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default withRouter(LandingPage)
