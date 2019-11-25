5f1450384a34e69eea6a1b054aa5199429d41621
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-25 16:13:51 +0530

    designing the login page

--- public/src/components/Login.js
+++ public/src/components/Login.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import "../styles/login.css"
 
 class Login extends Component{
     constructor(){
@@ -6,7 +7,13 @@ class Login extends Component{
     }
     render(){
         return(
-            <h1>login pages</h1>
+            <div className = "login-page">
+                <div className = "login" > 
+                    <h1 className = "app-name"> Hop On </h1>
+                    <button type="button" className = "take-ride" >Take a <br />Ride</button>
+                    <button type="button" className = "share-ride" >Share a <br />Ride</button>
+                </div>
+            </div>
         )
     }
 }
