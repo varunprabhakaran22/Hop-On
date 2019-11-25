8e11895831b41eb40365e2b931c02163c7dfd727
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-25 16:14:31 +0530

    creating the new component

---
+++ public/src/components/TakeARide.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import "../styles/takeARide"
+
+class TakeARide extends Component{
+    constructor(){
+        super()
+    }
+
+    render(){
+        return(
+            
+        )
+    }
+}
