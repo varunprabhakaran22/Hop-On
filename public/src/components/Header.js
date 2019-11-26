ed4482bebbcd83b71d1100cb1da9d291c25ca018
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-26 23:56:31 +0530

    creating the new component called header

---
+++ public/src/components/Header.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+import "../styles/takeARide.css"
+
+
+class TakeARide extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+            <div className = "header" > 
+            <div className = "menu-header">
+                <div className = "menu"> </div>  
+                <div className = "menu"></div>
+                <div className = "menu" ></div>
+            </div>
+            <div className = "title">
+                Hop On 
+            </div>
+        </div>
+        )
+    }
