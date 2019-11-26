befb5ecaa7c8617f0bd21b29defa50aa2b0e6de9
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-27 01:57:55 +0530

    creating the server page

---
+++ server/index.js
@@ -0,0 +1,25 @@
+//importing all the required services 
+const express = require('express');
+const mongo = require('mongodb').MongoClient;
+const db = require('./config/config');
+const port = process.env.PORT || 8000;
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+app.use(express.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(body());
+app.use(cors());
+
+//connecting the mongodb with server
+mongo.connect(db.url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+    if (err)
+        return console.log(err);
+    const database = db.db("UserData")
+    require('./app/routes')(app, database);
+    app.listen(port, () => {
+        console.log('Server is ready');
+    });
+})
