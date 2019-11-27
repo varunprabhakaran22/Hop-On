5517dd40512315efb2643036fe7fa42f876167d4
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-27 18:23:24 +0530

    creating the component for sharing the ride

---
+++ public/src/components/ShareARide.js
@@ -0,0 +1,49 @@
+import React,{Component} from 'react';
+import Header from "./Header"
+import SearchForShareARide from "./SearchForShareARide";
+import Footer from './Footer';
+import axios from "axios"
+import "../styles/takeARide.css"
+
+class ShareARide extends Component{
+    constructor(){
+        super()
+        this.state ={
+            userName: "",
+            mobileNum:"",
+            carName:"",
+            carNum:"",
+            vacantSpace:"",
+            source:"",
+            destination:""
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange = (event) => {
+        const {name, value} = event.target
+        this.setState({
+            [name]: value
+        })
+    }
+ 
+    async handleSubmit(){
+        const data = this.state
+        console.log(userName+ "" + mobileNum + "" + "" + carName + "" + carNum + "" + vacantSpace + "" +source + "" +destination);
+        await axios.post("http://localhost:8000/share/aRide",data)
+        .then(() =>console.log("axios"))
+        .catch((err) => console.log(err))
+    }
+
+    render(){
+        return(
+            <div>
+                <Header />
+                <SearchForShareARide state={this.state} handleChange={this.handleChange}/>
+                <Footer submitHandler={this.handleSubmit}/>
+            </div>
+        )
+    }
+}
+
+export default ShareARide;
