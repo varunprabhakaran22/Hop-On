9ba26ccf5a801c04b6823245740c315960f5d37a
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-27 15:44:33 +0530

    getting the user datas as props

--- public/src/components/SearchArea.js
+++ public/src/components/SearchArea.js
@@ -9,15 +9,41 @@ class SearchArea extends Component{
     render(){
         return(
             <div>
-                <div className = "profile">
-                    <input type = "text"  className="user-name" placeholder = "Name" ></input>
-                    <input type ="text"  className = "mobile-no" placeholder = "Mobile number" ></input>
-                </div>
-                <div className = "search-area">
-                    <input type="text" className = "pick-up" placeholder = "Pick up Location" />
-                    <input type="text" className = "drop" placeholder = "Drop Location" />
-                </div>
+            <div className = "profile">
+                <input type = "text"  
+                    className="user-name" 
+                    name="userName"   
+                    value={this.props.state.userName} 
+                    placeholder = "Name" 
+                    onChange = {event => this.props.handleChange(event)} 
+                />
+                <input type ="text"  
+                    className = "mobile-no" 
+                    name = "mobileNum"  
+                    value={this.props.state.mobileNum} 
+                    placeholder = "Mobile number" 
+                    onChange = {event => this.props.handleChange(event)} 
+                />
             </div>
+{/* <h1> {this.state.userName } {this.state.mobileNum}</h1> */}
+            <div className = "search-area">
+                <input type="text" 
+                    className = "pick-up" 
+                    name = "source"  
+                    value={this.props.state.source} 
+                    placeholder = "Pick up Location" 
+                    onChange = {event => this.props.handleChange(event)} 
+                />
+                <input type="text" 
+                    className = "drop" 
+                    name = "destination"  
+                    value={this.props.state.destination} 
+                    placeholder = "Drop Location" 
+                    onChange = {event => this.props.handleChange(event)}
+                />
+            </div>
+            {/* <h1> {this.state.source } {this.state.destination}</h1> */}
+        </div>
         )
     }
 }
