c6dc101b43e3eb8559f58415fa17f9c77e71f2d5
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-27 17:40:08 +0530

    creating the route to receive the data

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 //importing all the required services 
 const express = require('express');
 const mongo = require('mongodb').MongoClient;
-const db = require('./config/config');
+// const db = require('./config/config');
 const port = process.env.PORT || 8000;
 const app = express();
 const bodyParser = require('body-parser');
@@ -11,16 +11,23 @@ app.use(express.urlencoded({extended:true}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(body());
+app.use(bodyParser());
 app.use(cors());
 
 //connecting the mongodb with server
-mongo.connect(db.url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
-    if (err)
-        return console.log(err);
-    const database = db.db("UserData")
-    require('./app/routes')(app, database);
-    app.listen(port, () => {
-        console.log('Server is ready');
-    });
+// mongo.connect(db.url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+//     if (err)
+//         return console.log(err);
+//     const database = db.db("UserData")
+//     require('./app/routes')(app, database);
+//    
+// })
+
+app.post("/takeride", (req, res) => {
+    console.log(req.body); 
 })
+
+
+app.listen(port, () => {
+    console.log('Server is ready');
+});
