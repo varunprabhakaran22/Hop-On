04617c2156f5ded5d68e149f923f647fa5bed3d5
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-28 23:10:57 +0530

    changing the route

--- public/src/components/LandingPage.js
+++ public/src/components/LandingPage.js
@@ -6,12 +6,13 @@ class LandingPage extends Component{
         super()
     }
     render(){
+        console.log("landing page " + this.props.state);
         return(
             <div className = "landing-page">
                 <div className = "landing" > 
                     <h1 className = "app-name"> Hop On </h1>
-                    <button type="button" className = "take-ride" onClick={()=>this.props.history.push("/takearide")}>Take a <br />Ride</button>
-                    <button type="button" className = "share-ride" onClick = {() => this.props.history.push("/sharearide")} >Share a <br />Ride</button>
+                    <button type="button" className = "take-ride" onClick={()=>this.props.history.push("/takearide") }>Take a <br />Ride</button>
+                    <button type="button" className = "share-ride" onClick = {() => this.props.history.push("/sharearide") } >Share a <br />Ride</button>
                 </div>
             </div>
         )
