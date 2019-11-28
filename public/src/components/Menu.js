8eae794702d8091a9f5251faab4e7e66aa8da9e6
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-28 17:30:48 +0530

    adding the firebase logout button

--- public/src/components/Menu.js
+++ public/src/components/Menu.js
@@ -1,6 +1,10 @@
 import React,{Component} from 'react';
 import "../styles/takeARide.css";
 import { withRouter } from 'react-router-dom'
+import firebase from "firebase"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+
+
 class Menu extends Component{
     constructor(){
         super()
@@ -11,7 +15,7 @@ class Menu extends Component{
             <div>
                 <h1> Menu Printing</h1>
                 <h3>
-                  d
+                <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
                 </h3>
             </div>
         )
