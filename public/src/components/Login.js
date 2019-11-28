d6320929b65a5ad71063c4ca522131b8b8819daa
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-28 15:22:15 +0530

    indendation

--- public/src/components/Login.js
+++ public/src/components/Login.js
@@ -8,55 +8,57 @@ import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 
 
 firebase.initializeApp({
-    apiKey: "AIzaSyDxf4pvmS6Zpjv3Wof0SdVJajuVa95pA1k",
-    authDomain: "hop-on-6b5d5.firebaseapp.com"
-  })
+  apiKey: "AIzaSyDxf4pvmS6Zpjv3Wof0SdVJajuVa95pA1k",
+  authDomain: "hop-on-6b5d5.firebaseapp.com"
+})
   
 class Login extends Component {
-    state = { isSignedIn: false }
-    uiConfig = {
-      signInFlow: "popup",
-      signInOptions: [
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        // firebase.auth.GithubAuthProvider.PROVIDER_ID,
-        // firebase.auth.EmailAuthProvider.PROVIDER_ID
-      ],
-      callbacks: {
-        signInSuccess: () => false
-      }
+  state = { isSignedIn: false }
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      // firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
     }
+  }
   
-    componentDidMount = () => {
-      firebase.auth().onAuthStateChanged(user => {
-        this.setState({ isSignedIn: !!user })
-        console.log("user", user)
-      })
-    }
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user })
+      console.log("user", user)
+    })
+  }
   
-    render() {
-        return (
-            <div className="App">
-            {this.state.isSignedIn ? (
-                <span>
-                <div>Signed In!</div>
-                <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
-                <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
-                <img
-                    alt="profile picture"
-                    src={firebase.auth().currentUser.photoURL}
-                />
-                </span>
-            ) : (
-                <StyledFirebaseAuth
-                uiConfig={this.uiConfig}
-                firebaseAuth={firebase.auth()}
-                />
-            )}
-            </div>
+  render() {
+    return (
+      <div className="App">
+        {this.state.isSignedIn ? (
+          <span>
+            <div>Signed In!</div>
+            <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+            <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+            <img
+              alt="profile picture"
+              src={firebase.auth().currentUser.photoURL}
+            />
+          </span>
+          ) : 
+          (
+            <StyledFirebaseAuth
+              uiConfig={this.uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
+          )
+        }
+      </div>
     )
-    }
+  }
 }
 
 
