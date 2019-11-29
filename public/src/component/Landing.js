05292ecbafdbabb85f38f03a2d23a2edb050534d
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-30 00:08:18 +0530

    updating the component

--- public/src/component/Landing.js
+++ public/src/component/Landing.js
@@ -3,6 +3,8 @@ import "../styles/header.css"
 import { withRouter, Link } from 'react-router-dom'
 import firebase from "firebase"
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+import logo from '../asset/banner.jpg'; 
+
 
 class Landing extends Component{
     constructor(){
@@ -11,10 +13,10 @@ class Landing extends Component{
     render(){
         return(
             <div>
-                <div class="heroBanner">
-                    <img src="/banner.jpg" class="hbImg" />
-                    <div class="hbBtnSec">
-                    <a href="#" class="hbBtn">Find a ride</a>
+                <div className="heroBanner">
+                    <img src={logo} className="hbImg" />
+                    <div className="hbBtnSec">
+                        <Link to='/find/a/ride' className="hbBtn" > Find a ride </Link>
                     </div>
                 </div>
             </div>
