fb845cc66d654a0c35ad9e39c80bc3a45d1c2641
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-29 15:08:09 +0530

    rideresult component

---
+++ public/src/component/RideResult.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+import "../styles/header.css"
+import { withRouter } from 'react-router-dom'
+import firebase from "firebase"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+
+
+
+
+export default RideResult
