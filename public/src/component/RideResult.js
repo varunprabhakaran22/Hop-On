c065cbd04c747a9951931cec7e6533bbae9b610d
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-29 15:08:57 +0530

    creating the cart for the user

--- public/src/component/RideResult.js
+++ public/src/component/RideResult.js
@@ -5,6 +5,27 @@ import firebase from "firebase"
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 
 
-
-
-export default RideResult
+class RideResult extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+            <div className="container700 autocenter p9000">
+                <div className="rideCard">
+                    <div className="flexResponsive768">
+                        <div className="flex1">
+                            <div className="rcLabel">02:00</div>
+                            <div className="rcLabel">09:10</div>
+                        </div>
+                        <div className="flexCent2"></div>
+                        <div className="flex7">
+                            <div className="flex flexJustifyBetween">
+                                <div className="rcLabel">Bengaluru</div>
+                                <div className="rcAmt">Rs.750.00</div>
+                            </div>
+                            <div className="flex flexJustifyBetween">
+                                <div className="rcLabel">Coimbatore</div>
+                            </div>
+                        </div>
+                    </div>
