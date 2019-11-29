d412c0a09fb297266e8740db8c25b9e1d4c5ad04
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-30 00:58:02 +0530

    Adding the header component

--- public/src/component/RideResult.js
+++ public/src/component/RideResult.js
@@ -3,6 +3,7 @@ import "../styles/header.css"
 import { withRouter } from 'react-router-dom'
 import firebase from "firebase"
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+import Header from "./Header"
 
 
 class RideResult extends Component{
@@ -11,62 +12,65 @@ class RideResult extends Component{
     }
     render(){
         return(
-            <div className="container700 autocenter p9000">
-                <div className="rideCard">
-                    <div className="flexResponsive768">
-                        <div className="flex1">
-                            <div className="rcLabel">02:00</div>
-                            <div className="rcLabel">09:10</div>
-                        </div>
-                        <div className="flexCent2"></div>
-                        <div className="flex7">
-                            <div className="flex flexJustifyBetween">
-                                <div className="rcLabel">Bengaluru</div>
-                                <div className="rcAmt">Rs.750.00</div>
-                            </div>
-                            <div className="flex flexJustifyBetween">
-                                <div className="rcLabel">Coimbatore</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="p3000">
+            <div>
+                <Header />
+                <div className="container700 autocenter p9000">
+                    <div className="rideCard">
                         <div className="flexResponsive768">
                             <div className="flex1">
-                                <img src="profile.jpeg" className="rcImg" />
+                                <div className="rcLabel">02:00</div>
+                                <div className="rcLabel">09:10</div>
                             </div>
                             <div className="flexCent2"></div>
-                            <div className="flex7 flex flexJustifyBetween flexAlignItemsCenter">
-                                <div className="rcLabel">Arvind</div>
-                                <a href="" className="rcLink">View</a>
+                            <div className="flex7">
+                                <div className="flex flexJustifyBetween">
+                                    <div className="rcLabel">Bengaluru</div>
+                                    <div className="rcAmt">Rs.750.00</div>
+                                </div>
+                                <div className="flex flexJustifyBetween">
+                                    <div className="rcLabel">Coimbatore</div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>                <div className="rideCard">
-                    <div className="flexResponsive768">
-                        <div className="flex1">
-                            <div className="rcLabel">02:00</div>
-                            <div className="rcLabel">09:10</div>
-                        </div>
-                        <div className="flexCent2"></div>
-                        <div className="flex7">
-                            <div className="flex flexJustifyBetween">
-                                <div className="rcLabel">Bengaluru</div>
-                                <div className="rcAmt">Rs.750.00</div>
-                            </div>
-                            <div className="flex flexJustifyBetween">
-                                <div className="rcLabel">Coimbatore</div>
+                        <div className="p3000">
+                            <div className="flexResponsive768">
+                                <div className="flex1">
+                                    <img src="profile.jpeg" className="rcImg" />
+                                </div>
+                                <div className="flexCent2"></div>
+                                <div className="flex7 flex flexJustifyBetween flexAlignItemsCenter">
+                                    <div className="rcLabel">Arvind</div>
+                                    <a href="" className="rcLink">View</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="p3000">
+                    </div>                <div className="rideCard">
                         <div className="flexResponsive768">
                             <div className="flex1">
-                                <img src="profile.jpeg" className="rcImg" />
+                                <div className="rcLabel">02:00</div>
+                                <div className="rcLabel">09:10</div>
                             </div>
                             <div className="flexCent2"></div>
-                            <div className="flex7 flex flexJustifyBetween flexAlignItemsCenter">
-                                <div className="rcLabel">Arvind</div>
-                                <a href="" className="rcLink">View</a>
+                            <div className="flex7">
+                                <div className="flex flexJustifyBetween">
+                                    <div className="rcLabel">Bengaluru</div>
+                                    <div className="rcAmt">Rs.750.00</div>
+                                </div>
+                                <div className="flex flexJustifyBetween">
+                                    <div className="rcLabel">Coimbatore</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p3000">
+                            <div className="flexResponsive768">
+                                <div className="flex1">
+                                    <img src="profile.jpeg" className="rcImg" />
+                                </div>
+                                <div className="flexCent2"></div>
+                                <div className="flex7 flex flexJustifyBetween flexAlignItemsCenter">
+                                    <div className="rcLabel">Arvind</div>
+                                    <a href="" className="rcLink">View</a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -75,10 +79,5 @@ class RideResult extends Component{
         )
     }
 }
-
-
-
-
-
 
 export default RideResult
