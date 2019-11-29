440bb4325c1776b2384dec5f95bfd4fb41234805
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-29 15:32:38 +0530

    creating the component for search area

---
+++ public/src/component/SearchComponent.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+import "../styles/header.css"
+import { withRouter } from 'react-router-dom'
+import firebase from "firebase"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+
+
+class SearchComponent extends Component{
+    constructor(){
+        super()
+    }
+    
+}
+
+export default SearchComponent
+
+
+
+
