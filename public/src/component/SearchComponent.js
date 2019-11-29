aec7a1d4ffb4a14e2b7ab95816403d521795dc6f
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-29 15:33:10 +0530

    Adding the required fields to the component

--- public/src/component/SearchComponent.js
+++ public/src/component/SearchComponent.js
@@ -9,7 +9,27 @@ class SearchComponent extends Component{
     constructor(){
         super()
     }
-    
+    render(){
+        return(
+            <div>
+                <div className="container700 autocenter p9000">
+                    <div className="textCenter">
+                        <h1>Find a ride</h1>
+                    </div>
+                    <div className="">
+                        <form className="form">
+                            <input type="text" className="frmField" placeholder="Leaving from" />
+                            <input type="text" className="frmField" placeholder="Going to" />
+                            <input type="datetime-local" name="date" />
+                            <div className="textCenter">
+                                <input type="submit" className="frmBtn" value="Find a Ride" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default SearchComponent
