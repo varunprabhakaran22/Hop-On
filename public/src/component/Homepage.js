a118140a3db57b7a37fe9c7058f79c67073b6633
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-29 14:06:48 +0530

    creating  the homepage component

---
+++ public/src/component/Homepage.js
@@ -0,0 +1,32 @@
+import React,{Component} from 'react';
+import "../styles/main.css"
+import { withRouter } from 'react-router-dom'
+import Header from './Header';
+
+
+
+export default Homepage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    <div class="heroBanner">
+//      <img src="/banner.jpg" class="hbImg" />
+//      <div class="hbBtnSec">
+//        <a href="#" class="hbBtn">Find a ride</a>
+//      </div>
+//    </div>
