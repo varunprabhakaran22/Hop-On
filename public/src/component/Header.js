c166de84af00579c889805ca5c4be8a5fb9aac9a
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-29 13:57:34 +0530

    creating the header component

--- public/src/component/Header.js
+++ public/src/component/Header.js
@@ -4,3 +4,16 @@ import { withRouter, Link } from 'react-router-dom'
 import firebase from "firebase"
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 
+
+class Header extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+            
+        )
+    }
+}
+
+export default Header
