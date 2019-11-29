e553ac7d63754bf8e1b2174f45d72aecc309d1ec
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-29 14:54:44 +0530

    creating the landing component

---
+++ public/src/component/Landing.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import "../styles/header.css"
+import { withRouter, Link } from 'react-router-dom'
+import firebase from "firebase"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+
+
+
+
+export default Landing
+
+
