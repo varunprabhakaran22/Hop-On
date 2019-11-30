bbf430c98897b9f837e2cd25b1183f2ec6a462b1
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-30 14:23:56 +0530

    creating the  route file to redirect

--- public/src/components/Login.js
+++ public/src/components/Login.js
@@ -1,67 +1,67 @@
-import React,{Component} from 'react';
-import FacebookLogin from 'react-facebook-login';
-import { GoogleLogin } from 'react-google-login';
-import cors from "cors";
-import firebase from "firebase"
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
-import LandingPage from './LandingPage';
-import Menu from "./Menu";
-import PostToServer from "./PostToServer";
+// import React,{Component} from 'react';
+// import FacebookLogin from 'react-facebook-login';
+// import { GoogleLogin } from 'react-google-login';
+// import cors from "cors";
+// import firebase from "firebase"
+// import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+// import LandingPage from './LandingPage';
+// import Menu from "./Menu";
+// import PostToServer from "./PostToServer";
 
-firebase.initializeApp({
-  apiKey: "AIzaSyDxf4pvmS6Zpjv3Wof0SdVJajuVa95pA1k",
-  authDomain: "hop-on-6b5d5.firebaseapp.com"
-})
+// firebase.initializeApp({
+//   apiKey: "AIzaSyDxf4pvmS6Zpjv3Wof0SdVJajuVa95pA1k",
+//   authDomain: "hop-on-6b5d5.firebaseapp.com"
+// })
 
 
-class Login extends Component {
-  state = { 
-    isSignedIn: false,
-    userDetails :{},
-    userEmail: "",
-  }
-  uiConfig = {
-    signInFlow: "popup",
-    signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      // firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      // firebase.auth.EmailAuthProvider.PROVIDER_ID
-    ],
-    callbacks: {
-      signInSuccess: () => false
-    }
-  }
+// class Login extends Component {
+//   state = { 
+//     isSignedIn: false,
+//     userDetails :{},
+//     userEmail: "",
+//   }
+//   uiConfig = {
+//     signInFlow: "popup",
+//     signInOptions: [
+//     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+//       // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+//       // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+//       // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+//       // firebase.auth.EmailAuthProvider.PROVIDER_ID
+//     ],
+//     callbacks: {
+//       signInSuccess: () => false
+//     }
+//   }
   
-  componentDidMount = () => {
-    firebase.auth().onAuthStateChanged(user => {
-      this.setState({ isSignedIn: !!user,userDetails:user})
-    }) 
-  }
+//   componentDidMount = () => {
+//     firebase.auth().onAuthStateChanged(user => {
+//       this.setState({ isSignedIn: !!user,userDetails:user})
+//     }) 
+//   }
   
-  render() {
-    const { isSignedIn,userDetails } =this.state
-    return (
-      <div className="App">
-        { isSignedIn ? (
-          <div>
-            < Menu  userDetails = { userDetails } />
-            <PostToServer userDetails = { userDetails} />
-            <LandingPage state = {this.state.isSignedIn}/>
-          </div>
-          ) : 
-          (
-            <StyledFirebaseAuth
-              uiConfig={this.uiConfig}
-              firebaseAuth={firebase.auth()}
-            />
-          )
-        }
-      </div>
-    )
-  }
-}
+//   render() {
+//     const { isSignedIn,userDetails } =this.state
+//     return (
+//       <div className="App">
+//         { isSignedIn ? (
+//           <div>
+//             < Menu  userDetails = { userDetails } />
+//             <PostToServer userDetails = { userDetails} />
+//             <LandingPage state = {this.state.isSignedIn}/>
+//           </div>
+//           ) : 
+//           (
+//             <StyledFirebaseAuth
+//               uiConfig={this.uiConfig}
+//               firebaseAuth={firebase.auth()}
+//             />
+//           )
+//         }
+//       </div>
+//     )
+//   }
+// }
 
 
-export default Login
+//  export default Login
