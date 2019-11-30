a2945e939702ce4d8a209c46bf0772b27a132f3e
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-30 14:22:32 +0530

    redirecting to other component

--- public/src/App.js
+++ public/src/App.js
@@ -4,7 +4,7 @@ import LandingPage from "./components/LandingPage"
 import TakeARide from "./components/TakeARide"
 import { Route,Switch,Redirect } from 'react-router-dom'
 import ShareARide from './components/ShareARide';
-import Login from './components/Login';
+import Login from './component/Login'
 import Homepage from "./component/Homepage"
 import FindRide from "./component/FindRide"
 import ShareRide from "./component/ShareRide"
@@ -22,8 +22,9 @@ class App extends Component {
     return (
       <div>
         <Switch>
-          <Route exact path="/" component={Homepage }/>  
-          <Route exact path="/find/a/ride" component={FindRide }/>
+          <Route exact path="/" component={Homepage }/>
+          <Route exact path="/login" component={Login }/>  
+          <Route exact path="/a/rifind/de" component={FindRide }/>
           <Route exact path="/search/a/ride" component={ShareRide}/>
           <Route exact path="/ride/result" component={RideResult}/>
           {/* <Route exact path="/result" component={ShareRide}/> */}
